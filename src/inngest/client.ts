50251fa4d87b7bad5dbc9533d74f0c3ba0f88035
ndwumfuoh5-debug <ndwumfuoh5@gmail.com>
2026-09-01 17:33:13 -0700

    Update client.ts

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 
 // Create Inngest client
 export const inngest = new Inngest({
-  id: process.env.INNGEST_APP_ID,
+  id: "sourcevc",
   eventKey: process.env.INNGEST_EVENT_KEY || "local",
   // For production, signing key is required to verify webhook requests from Inngest
   ...(process.env.NODE_ENV === "production" && {
